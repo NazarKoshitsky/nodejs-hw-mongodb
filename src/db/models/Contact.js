e901774ccabf6f79a03b8404b291c859f9824027
Nazar Koshytskyi <77495036+NazarKoshitsky@users.noreply.github.com>
2024-07-11 17:40:47 +0200

    delete versionKey

--- src/db/models/Contact.js
+++ src/db/models/Contact.js
@@ -1,35 +1,38 @@
 import { Schema, model } from 'mongoose';
 
-const contactSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      requried: false,
+    },
+    isFavourite: {
+      type: Boolean,
+      default: false,
+    },
+    contactType: {
+      type: String,
+      enum: ['work', 'home', 'personal'],
+      default: 'personal',
+    },
+    createdAt: {
+      type: Date,
+      timestamps: true,
+    },
+    updatedAt: {
+      type: Date,
+      timestamps: true,
+    },
   },
-  phoneNumber: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    requried: false,
-  },
-  isFavourite: {
-    type: Boolean,
-    default: false,
-  },
-  contactType: {
-    type: String,
-    enum: ['work', 'home', 'personal'],
-    default: 'personal',
-  },
-  createdAt: {
-    type: Date,
-    timestamps: true,
-  },
-  updatedAt: {
-    type: Date,
-    timestamps: true,
-  },
-});
+  { versionKey: false, timestamps: true },
+);
 
 export const Contact = model('contact', contactSchema);
